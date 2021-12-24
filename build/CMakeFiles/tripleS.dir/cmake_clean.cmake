246817bedc17b694335f9141ea31070421c4d0b2
trackpadpro <trackpadpro>
2021-12-24 18:04:35 -0500

    add initial CMake files

---
+++ build/CMakeFiles/tripleS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tripleS.dir/main.cpp.o"
+  "CMakeFiles/tripleS.dir/main.cpp.o.d"
+  "libtripleS.dll.a"
+  "tripleS.exe"
+  "tripleS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tripleS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/tripleS.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tripleS.
+# This may be replaced when dependencies are built.
