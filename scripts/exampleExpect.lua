46e6a95f4ba1d0c0a6032942a4bd8ee9fd19fc70
Valentin Richter <valentinsrichter@gmail.com>
2022-04-09 07:29:52 -0400

    add hymkor/expect@a66b933641fb76685ed34f9a14a2bf49d3060ecc auto-run code

---
+++ scripts/exampleExpect.lua
@@ -0,0 +1,12 @@
+echo(true)
+if spawn([[..\..\tppsss\tripleS.exe]]) then
+    expect("[y/n]")
+    echo(false)
+    send("n\r")
+    expect("Online")
+    send("setup\r")
+    expect("cookie:")
+    send("${{secrets.STEAM_LOGIN_COOKIE}}\r")
+    expect("steamLoginSecure set")
+    send("break\r")
+end
---
+++ scripts/exampleExpectLua.ps1
@@ -0,0 +1,3 @@
+$pathToScripts = $MyInvocation.MyCommand.Path
+
+expect.exe $pathToScripts\..\exampleExpect.lua
